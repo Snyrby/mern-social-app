b9fcdad887126f68f9dd5fca91258de7a2e37ec2
Shawn Ruby <snyrby@gmail.com>
2023-06-13 07:04:39 -0700

    Update navbar.jsx

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -69,8 +69,28 @@ const navbar = () => {
       {isNonMobileScreens ? (
       <FlexBetween gap="2rem">
         <IconButton onClick={() => dispatch(setMode())}>
-          
+          {theme.palette.mode === "dark" ? (
+            <DarkMode sx={{ fontSize: "25px" }} />
+          ) : (
+            <LightMode sx={{color: dark, fontSize: "25px"}} />
+          )}
         </IconButton>
+        <Message sx={{ fontSize: "25px" }} />
+        <Notifications sx={{ fontSize: "25px" }} />
+        <Help sx={{ fontSize: "25px" }} />
+        <FormControl variant="standard" value={fullName}>
+          <Select 
+          value={fullName} 
+          sx={{
+            backgroundColor: neutralLight, 
+            width: "150px",
+            borderRadius: "0.25rem",
+            p: "0.25rem 1rem",
+            "& .Mui"
+            }}>
+
+          </Select>
+        </FormControl>
       </FlexBetween>
       ) : (<IconButton></IconButton>)}
     </FlexBetween>
