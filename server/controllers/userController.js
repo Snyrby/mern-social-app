1ae3d1e3f8f5278e605c7bffd0803755fdf8bd0c
Shawn Ruby <snyrby@gmail.com>
2023-07-06 19:12:53 -0700

    Small change

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -72,11 +72,11 @@ const uploadImage = async (req, res) => {
     throw new CustomError.BadRequestError("Please Upload Image");
   }
 
-  const maxSize = 1024 * 1024 * 5;
+  const maxSize = 1024 * 1024 * 10;
 
   if (productImage.size > maxSize) {
     throw new CustomError.BadRequestError(
-      "Please upload image smaller than 5MB"
+      "Please upload image smaller than 10MB"
     );
   }
 
